5ebeb486bbddc5a2ae02522769a82ebd8937181e
Alvaro Polo <apoloval@gmail.com>
2013-04-17 19:07:21 +0200

    Fix copyright notes

--- src/ui/event.cc
+++ src/ui/event.cc
@@ -1,5 +1,6 @@
-
 /* This file is part of Grog.
+ *
+ * Copyright (c) 2013 Alvaro Polo
  *
  * Grog is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
